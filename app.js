c3d6f236387c8e8bfc6cd4fdc7686746ccb666f4
mattcam <matt@mattcameron.me>
2018-08-12 08:43:48 +1000

    Change format of array passed to target_data, closes #1
    Changed example to support `@tensorflow/tfjs` `0.12.4`, previously was `0.9.1`

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ model.add(tf.layers.dense({units: 1, activation: 'sigmoid',inputShape: [10]}));
 model.compile({loss: 'meanSquaredError', optimizer: 'rmsprop'});
 
 const training_data = tf.tensor2d([[0,0],[0,1],[1,0],[1,1]]);
-const target_data = tf.tensor2d([0,1,1,0]);
+const target_data = tf.tensor2d([[0],[1],[1],[0]]);
 
 for (let i = 1; i < 100 ; ++i) {
  var h = await model.fit(training_data, target_data, {epochs: 30});
